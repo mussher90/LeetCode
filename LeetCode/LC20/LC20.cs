3f02099751ef0cbbf470d5fb664a51ad9153c7a4
= <=>
2022-09-16 17:54:26 +1200

    LC 20 done

---
+++ LeetCode/LC20/LC20.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LC20
+{
+    public static class LC20
+    {
+        public static bool IsValid(string parentheses)
+        {
+            bool valid;
+            do
+            {
+                var length = parentheses.Length;
+
+                if (parentheses.Contains("()"))
+                {
+                    parentheses = parentheses.Replace("()", "");
+                }
+
+                if (parentheses.Contains("{}"))
+                {
+                    parentheses = parentheses.Replace("{}", "");
+                }
+
+                if (parentheses.Contains("[]"))
+                {
+                    parentheses = parentheses.Replace("[]", "");
+                }
+
+                valid = parentheses.Length < length;
+
+                if(parentheses == "")
+                {
+                    return true;
+                }
+
+            } while (valid);
+
+            return false;
+        }
+    }
+}
